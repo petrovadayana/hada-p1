f6b073cb34442e7d902f237cd213f26522832ed8
Dayana Petrova <dpt9@gcloud.ua.es>
2021-02-18 21:17:02 +0100

    Program.cs modified

--- hada-p1/Program.cs
+++ hada-p1/Program.cs
@@ -8,8 +8,58 @@ namespace hada_p1
 {
     class Program
     {
+        private static bool isActive = false;
+        private static char line;
+        public static char Line
+        {
+            set
+            {
+                line = value;
+                isActive = value == 'q' ? false : true;
+            }
+            get
+            {
+                return line;
+            }
+        }
+        static double getValue(char line, double valor)
+        {
+            switch (line)
+            {
+                case 'm':
+                    return HadaP1.Minutes2Seconds(valor);
+                case 's':
+                    return HadaP1.Seconds2Minutes(valor);
+                default:
+                    return -1;
+            }
+        }
+
+        static char getType()
+        {
+            Console.Write("Valor a introducir: 's' (segundos) o 'm' (minutos) / q Quit: ");
+            return Console.ReadLine()[0];
+        }
+
         static void Main(string[] args)
         {
+            do
+            {
+                Line = getType();
+                if (isActive)
+                {
+                    Console.Write("Introduce valor a convertir: ");
+                    string valueInput = Console.ReadLine();
+                    double value = double.Parse(valueInput);
+
+                    double result = getValue(line, value);
+
+                    if (result != -1)
+                    {
+                        Console.WriteLine("Result: " + result);
+                    }
+                }
+            } while (isActive);
         }
     }
 }
