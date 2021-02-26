589a9553e08272ac14833290b606daee14d46852
Dayana Petrova <dpt9@gcloud.ua.es>
2021-02-26 21:04:28 +0100

    Added Hours2Minutes

--- hada-p1/HadaP1.cs
+++ hada-p1/HadaP1.cs
@@ -16,7 +16,12 @@ namespace hada_p1
 
         public static double Minutes2Seconds (double m)
         {
-            return m == 0 ? 0: m * 60; /// si min no es 0 se mult por 60 para pasarlo a seg
+            return m == 0 ? 0: m * 60; /// si min no es 0 se mult por 60 para pasarlo a segundos
+        }
+
+        public static double Hours2Minutes (double h)
+        {
+            return h == 0 ? 0: h * 60; /// si hours no es 0, lo mult por 60 para pasarlo a min
         }
     }
 }
